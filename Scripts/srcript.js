2a8a8b96c5d839190bddf12d71ec16a6126b613a
Miguel Chiara <miguel.chiara.aguilaramat.com>
2020-11-24 09:33:32 +0100

    COMMIT

--- Scripts/srcript.js
+++ Scripts/srcript.js
@@ -1,22 +1,55 @@
 //Muestra el formulario que fue ocultado con css por eso usamos la clase jquery-3.1.1.min.js
-$('.toggle').click(function () {
+$('.toggle').click(extender);
+
+function extender() {
     $('.contformulario').animate({
         height: "toggle",
         'padding-top': 'toggle',
         'padding-bottom': 'toggle',
         opacity: 'toggle'
     }, "slow");
-});
+};
+
+
+
 
 //Manda los datos al log (Simula subir a una base de datos)
 document.querySelector("#registrarse").addEventListener("click", function () {
     var intput = document.getElementsByTagName("input");
-    console.log(obtenerdatos()[0]);
-    console.log(obtenerdatos()[1]);
-    console.log(obtenerdatos()[2]);
-    console.log(obtenerdatos()[3]);
-    console.log(obtenerdatos()[4]);
-    console.log(obtenerdatos()[5]);
+    var correcto = true;
+    if (obtenerdatos()[0] == "") {
+        correcto = false;
+    }
+    if (obtenerdatos()[1] == "") {
+        correcto = false;
+    }
+    if (obtenerdatos()[2] == "") {
+        correcto = false;
+    }
+    if (obtenerdatos()[3] == "") {
+        correcto = false;
+    }
+    if (obtenerdatos()[4] == "") {
+        correcto = false;
+    }
+    if (obtenerdatos()[4] == "") {
+        correcto = false;
+    }
+    if (obtenerdatos()[5] == "") {
+        correcto = false;
+    }
+    if (correcto) {
+        console.log(obtenerdatos()[0]);
+        console.log(obtenerdatos()[1]);
+        console.log(obtenerdatos()[2]);
+        console.log(obtenerdatos()[3]);
+        console.log(obtenerdatos()[4]);
+        console.log(obtenerdatos()[5]);
+        extender();
+        alert("El registro se ha efectuado con exito");
+    } else {
+        alert("No se ha podido completar el registro");
+    }
 });
 
 //Comprueba si el usuario y la contraseña son correctos comprobando en el vector
@@ -28,10 +61,10 @@ document.querySelector("#inicio").addEventListener("click", function () {
         if (usuario == obtenerdatos()[0] && password == obtenerdatos()[1]) {
             //Se abre en una ventana nueva para que se vea la funciona lidad del formulario
             window.open("Tareas.html", "nombre_de_la_ventana", "width=1000,height=1500");
-        }else{
+        } else {
             alert("Usuario o contraseña incorrecto");
         }
-    }else{
+    } else {
         alert("Introduce datos");
     }
 
